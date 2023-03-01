f93868d76826de25b670b088ce3b2377962a2d15
patrcoff <90782615+patrcoff@users.noreply.github.com>
2023-03-01 15:58:54 +0000

    deleting django but keeping models files for ref

--- medecines/admin.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-
-# Register your models here.
-from .models import Dosage, Frequency, Medecine, MedecineAssignment
-
-admin.site.register(Dosage)
-admin.site.register(Medecine)
-admin.site.register(Frequency)
-admin.site.register(MedecineAssignment)
--- pets/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-
-# Register your models here.
-from .models import Species, Breed, SizeClass
-
-admin.site.register(Species)
-admin.site.register(Breed)
-admin.site.register(SizeClass)
